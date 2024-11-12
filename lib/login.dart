ba84e82e673bcae5f195ef638bea29b2fd1dbc81
Ayoola <ayoolaolaosebikan923@gmail.com>
2024-11-12 08:57:32 -0600

    final commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'questions_page.dart';
 
 // Pseudo database to store users
 List<Map<String, String>> users = [];
@@ -51,43 +50,71 @@ class _LoginPageState extends State<LoginPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            TextFormField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const CreateAccountPage()),
-                );
-              },
-              child: const Text('Create Account'),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              _message,
+            const SizedBox(height: 36),
+            const Text(
+              'Welcome to the Ethical App',
               style: TextStyle(
-                color:
-                    _message == 'Login successful!' ? Colors.green : Colors.red,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: const Text('Login'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CreateAccountPage()),
+                      );
+                    },
+                    child: const Text('Create Account'),
+                  ),
+                  //const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ForgotPasswordPage()),
+                      );
+                    },
+                    child: const Text('Forgot Password?'),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    _message,
+                    style: TextStyle(
+                      color: _message == 'Login successful!'
+                          ? Colors.green
+                          : Colors.red,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -160,6 +187,83 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
               _message,
               style: TextStyle(
                 color: _message == 'Account created successfully!'
+                    ? Colors.green
+                    : Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  String _message = '';
+
+  void _resetPassword() {
+    String username = _usernameController.text.trim();
+    String newPassword = _newPasswordController.text.trim();
+
+    bool userExists = users.any((user) => user['username'] == username);
+
+    setState(() {
+      if (userExists) {
+        // Find the user and reset the password
+        int userIndex =
+            users.indexWhere((user) => user['username'] == username);
+        users[userIndex]['password'] = newPassword;
+        _message = 'Password reset successfully!';
+      } else {
+        _message = 'Username not found!';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forgot Password'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _newPasswordController,
+              decoration: const InputDecoration(
+                labelText: 'New Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _resetPassword,
+              child: const Text('Reset Password'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _message,
+              style: TextStyle(
+                color: _message == 'Password reset successfully!'
                     ? Colors.green
                     : Colors.red,
               ),
